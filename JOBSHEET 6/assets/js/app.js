2470b0c9082ca14299e0002fa9ee1f9e6beb4a13
nashyra24 <140595105+nashyra24@users.noreply.github.com>
2026-09-16 16:23:44 +0700

    revert changes

--- JOBSHEET 6/assets/js/app.js
+++ JOBSHEET 6/assets/js/app.js
@@ -67,7 +67,7 @@ function initValidasiForm() {
         let valid = true;
 
         // Daftar field wajib isi
-        const fieldWajib = ["Nama Barang", "Peminjam", "Tanggal Dipinjam", "Tanggal Dikembalikan"];
+        const fieldWajib = ["judul", "nama", "pengarang", "no_anggota"];
 
         fieldWajib.forEach(function (namaField) {
             const input = form.querySelector("[name='" + namaField + "']");
